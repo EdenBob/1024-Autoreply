f4df823240513410899be8af5af19bbd8363a5ed
EdenBob <47360310+EdenBob@users.noreply.github.com>
2020-08-26 10:18:30 +0800

    Update 1024.py

--- 1024.py
+++ 1024.py
@@ -253,7 +253,7 @@ if __name__ == "__main__":
         auto.browse()
         auto.getreply()
         auto.getmatch()
-        sleeptime=random.randint(1024,1536)
+        sleeptime=random.randint(1024,1537)
         au=auto.postreply()
         if au=='回复成功':
             auto.debug('回复成功')
